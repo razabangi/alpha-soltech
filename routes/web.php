16ff7768f25ae4cda73fb6df2b5e703893c81a45
razabangi <muhammadrazabangi9@gmail.com>
2022-10-25 21:54:57 +0500

    remove vendor and node module from master

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/fb', function () {
+    $url = "https://scrape.deviyoinc.com/?query=need%20a%20php%20developer";
+    $response = Http::get($url)
+            ->json();
+    return view('fb', compact('response'));
+})->middleware(['auth', 'verified']);
+
+require __DIR__.'/auth.php';
