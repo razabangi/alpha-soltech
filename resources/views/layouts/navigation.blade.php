501895eba09354c1c9d17493e7f1ddc7bcf2ad93
razabangi <muhammadrazabangi9@gmail.com>
2022-10-26 16:24:30 +0500

    add facebook tab in responsive view

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,7 +15,7 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('facebook.index')" :active="request()->routeIs(['facebook', 'facebook.store'])">
+                    <x-nav-link :href="route('facebook.index')" :active="request()->routeIs(['facebook.index', 'facebook.store'])">
                         {{ __('Facebook') }}
                     </x-nav-link>
                 </div>
@@ -68,6 +68,9 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('facebook.index')" :active="request()->routeIs(['facebook.index', 'facebook.store'])">
+                {{ __('Facebook') }}
             </x-responsive-nav-link>
         </div>
 
